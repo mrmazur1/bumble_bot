5b65420dd1f6967b8b7090aa299cdddd1b64ea70
Michael Mazur <michaelrmaz@gmail.com>
2023-10-23 02:54:40 -0600

    mergeing scraper with main

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from scraper import scrapper
 
 
 if __name__ == "__main__":
-    # bl = bumbleLoader()
-    # bl.load()
-    # bl.start()
-    #cookieManager.run()
-    scr = scrapper()
-    url = input("url: ")
-    num = input("pics: ")
-    scr.scrape(url, int(num))
-    #print("done")
+    bl = bumbleLoader()
+    bl.load()
+    bl.start()
+    cookieManager.run()
+    print("done")
