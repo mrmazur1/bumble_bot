1470d261c63e188ce871f5b672467180f5c20583
Michael Mazur <michaelrmaz@gmail.com>
2023-10-22 23:45:13 -0600

    got scrapper working

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ if __name__ == "__main__":
     # bl.start()
     #cookieManager.run()
     scr = scrapper()
-    scr.scrape("https://www.bing.com/images/search?view=detailV2&ccid=t%2BKi%2B3mI&id=CE398FF1856491CA78ACB10B2F4A7E4E7A27D436&thid=OIP.t-Ki-3mI6ubupmliuaMGvwAAAA&mediaurl=https%3A%2F%2Ferniebufflo.files.wordpress.com%2F2017%2F03%2Fimg_2727.jpg%3Fw%3D300&exph=300&expw=300&q=kinda+ugly+woman+selfie+college+girl&form=IRPRST&ck=7963A5929FFBA3BF4C7F6C811C8BE6A6&selectedindex=3&ajaxhist=0&ajaxserp=0&pivotparams=insightsToken%3Dccid_SCrHAvas*cp_83A699FA87A9F83E3E9EC0697B469158*mid_3BA53A2F9488ACC3EC559AAE49CBABDC6B0F907C*simid_608014984258088731*thid_OIP.SCrHAvasx-z8R1hhuA9eyQAAAA&vt=0&sim=11&iss=VSI&simid=608010375747680179&cdnurl=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.b7e2a2fb7988eae6eea66962b9a306bf%3Frik%3DNtQnek5%252bSi8LsQ%26pid%3DImgRaw%26r%3D0")
+    url = input("url: ")
+    num = input("pics: ")
+    scr.scrape(url, int(num))
     #print("done")
--- scraper.py
+++ scraper.py
@@ -2,15 +2,16 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import uuid
+import requests
 
 class scrapper:
 
-    def scrape(self, url):
+    def scrape(self, url, nums):
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
@@ -18,16 +19,46 @@ class scrapper:
         edge_service = Service(edge_driver_path)
         driver = webdriver.Edge(service=edge_service)
         driver.get(url)
-        time.sleep(4)
+        time.sleep(3)
         try:
-            list_items = driver.find_elements(By.TAG_NAME, 'img')
+            driver.find_element(By.XPATH, '//*[@id="vs_images"]/div/div/ul/li[1]/div/div/div[1]/div/a/img').click()
+            img = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div/img')
             direc = "ad/"
             count = 0
-            for li in list_items:
-                if li.get_attribute('src') is not None and li.size['height'] > 0 and li.size['width'] > 0:
-                    li.screenshot(direc+str(count)+'.png')
-                    count+=1
+            urls = set()
+            end = time.time()+60
+            while count < nums:
+                #curr =
+                try:
+                    # if curr > end:
+                    #     break
+                    url = img.get_attribute('src')
+                    if img.get_attribute('src') is not None and img.size['height'] > 192 and img.size[
+                        'width'] > 192 and url not in urls:
+                        data = requests.get(url).content
+                        unique_filename = str(uuid.uuid4())
+                        full_path = os.path.join(direc, unique_filename)
+                        f = open(f'{full_path}.jpg', 'wb')
+                        f.write(data)
+                        f.close()
+                        urls.add(url)
+                        print(count)
+                        count += 1
+                    next = WebDriverWait(driver, 3).until(
+                        EC.presence_of_element_located((By.XPATH, '//*[@id="navr"]/span'))
+                    )
+                    next.click()
+                    img = WebDriverWait(driver, 3).until(
+                        EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div/img'))
+                    )
+                except Exception as e:
+                    driver.find_element(By.XPATH,
+                                        '//*[@id="vs_images"]/div/div/ul/li[1]/div/div/div[1]/div/a/img').click()
+                    img = WebDriverWait(driver, 3).until(
+                        EC.presence_of_element_located(
+                            (By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div/img'))
+                    )
+                    pass
         except Exception as e:
-            print("An error occurred:", e)
-        finally:
-            driver.quit()
+            print(e)
+
