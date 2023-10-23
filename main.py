7e0f54667c154db0d1ec0073ece7f970d1134a3f
Michael Mazur <michaelrmaz@gmail.com>
2023-10-23 03:44:10 -0600

    added trainer class to use simpleCNN

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-
+from simpleCNN import Trainer
 from bumbleLoader import bumbleLoader
 import cookieManager
 from scraper import scrapper
 
 
 if __name__ == "__main__":
-    bl = bumbleLoader()
-    bl.load()
-    bl.start()
-    cookieManager.run()
+    #TODO keep this code cause it loads the main thing
+
+    # bl = bumbleLoader()
+    # bl.load()
+    # bl.start()
+    # cookieManager.run()
+
+    trainer = Trainer()
+    trainer.train_model('model_test.pth', 'NN_data/hot_or_not_oct_23', 32, 2)
     print("done")
--- simpleCNN.py
+++ simpleCNN.py
@@ -38,7 +38,11 @@ class SimpleCNN(nn.Module):
         x = self.fc2(x)
         return x
 
-    def train_model(self, output_dir, data_directory, batch_size=4):
+class Trainer():
+    def __init__(self):
+        self.model = SimpleCNN()
+
+    def train_model(self, output_filename, data_directory, batch_size=4, epochs=8):
         # Define your ImageFolder dataset
         transform = myTransform().transform
         dataset = ImageFolder(data_directory, transform=transform)
@@ -57,3 +61,53 @@ class SimpleCNN(nn.Module):
         # Create DataLoader instances for training and validation
         train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
         val_loader = DataLoader(val_dataset, batch_size=batch_size)
+
+        # Loss function and optimizer
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+
+        # Training loop
+        train_losses = []
+        val_losses = []
+
+        for epoch in range(epochs):
+            self.model.train()  # Set the model to training mode
+            running_loss = 0.0
+
+            for batch_idx, data in enumerate(train_loader):
+                inputs, labels = data
+                optimizer.zero_grad()  # Zero the parameter gradients
+                outputs = self.model(inputs)  # Forward pass
+                loss = criterion(outputs, labels)  # Calculate the loss
+                loss.backward()  # Backpropagation
+                optimizer.step()  # Update weights
+
+                running_loss += loss.item()
+                print(f"Epoch [{epoch + 1}/{epochs}] "
+                      f"Batch [{batch_idx + 1}/{len(train_loader)}] "
+                      f"Loss: {running_loss / 10:.4f}")
+
+            # Calculate and store the average training loss for the epoch
+            average_train_loss = running_loss / len(train_loader)
+            train_losses.append(average_train_loss)
+
+            # Validation loop (evaluate the model on the validation set)
+            self.model.eval()  # Set the model to evaluation mode
+            running_loss = 0.0
+
+            with torch.no_grad():
+                for inputs, labels in val_loader:
+                    outputs = self.model(inputs)  # Forward pass
+                    loss = criterion(outputs, labels)  # Calculate the loss
+
+                    running_loss += loss.item()
+
+            # Calculate and store the average validation loss for the epoch
+            average_val_loss = running_loss / len(val_loader)
+            val_losses.append(average_val_loss)
+
+            # Print or log the training and validation losses for this epoch
+            print(
+                f"Epoch [{epoch + 1}/{epochs}] - Train Loss: {average_train_loss:.4f} - Val Loss: {average_val_loss:.4f}")
+
+        torch.save(self.model.state_dict(), output_filename)
