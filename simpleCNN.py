3da086315ff5caf04bf33e179f3f808dad577dfe
mrmazur1 <michaelrmaz@gmail.com>
2023-11-13 03:24:43 -0700

    small changes before checkout

--- simpleCNN.py
+++ simpleCNN.py
@@ -127,8 +127,10 @@ class Resnet_model:
 
     def train(self, output_filename, data_directory, batch_size=16, epochs=8):
         transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
@@ -150,7 +152,7 @@ class Resnet_model:
 
 
 
-        model = models.resnet50(pretrained=False)
+        model = models.resnet18(pretrained=False)
         use_cuda = torch.cuda.is_available()
         device = torch.device("cuda" if use_cuda else "cpu")
         model.to(device)
@@ -159,7 +161,7 @@ class Resnet_model:
         model.to(device)
 
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        optimizer = optim.adam(model.parameters(), lr=0.0005, momentum=0.9)
 
         # Training loop
         train_losses = []
