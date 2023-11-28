0c1da09ce5a019d7fab61468ddc5536d661a726a
mrmazur1 <michaelrmaz@gmail.com>
2023-11-28 05:39:18 -0700

    updated mydata

--- myData.py
+++ myData.py
@@ -3,7 +3,9 @@ from torchvision import transforms
 def get_architecture(type='res'):
     available = {
         'res': resnetData(),
-        'dense': denseData()
+        'dense': denseData(),
+        'google': googleData(),
+        'vgg':vggData()
     }
     if type in available:
         return available[type]
@@ -34,5 +36,31 @@ class denseData():
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+        self.class_labels = ['Hot', 'Not']
+
+class vggData():
+    def __init__(self):
+        self.transform = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.RandomPerspective(0.2),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        self.class_labels = ['Hot', 'Not']
+
+class googleData():
+    def __init__(self):
+        self.transform = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.RandomPerspective(0.2),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ])
         self.class_labels = ['Hot', 'Not']
