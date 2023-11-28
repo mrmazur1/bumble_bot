d7812f0219fb5fd92ece5b9476cf3357c8be35a8
mrmazur1 <michaelrmaz@gmail.com>
2023-11-28 00:07:06 -0700

    main shit

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
             print(f"restarting at count of {val}")
             bl.driver.get("https://www.google.com/")
             bl.driver.get("https://bumble.com")
+            bl.load()
             #bl.driver.quit()
             with open("web_page_source.html", "w", encoding="utf-8") as file:
                 file.write(html)
