9ee2dfcc072cff6812544d43fffa33bad27f616b
mrmazur1 <michaelrmaz@gmail.com>
2024-03-06 00:57:15 -0700

    small fix for restarting

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
             bl.driver.save_screenshot("web_page_screenshot.png")
             val,nlikes, ndislikes = bl.tracker, bl.numLikes, bl.numDislikes
             print(f"restarting at count of {val}")
-            # bl.driver.get("https://www.google.com/")
-            # bl.driver.get("https://bumble.com")
+            bl.driver.get("https://www.google.com/")
+            bl.driver.get("https://bumble.com")
             bl.load()
             bl.driver.quit()
             with open("web_page_source.html", "w", encoding="utf-8") as file:
