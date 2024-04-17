2541f9481ad0ba1affdcd048020a77153313db36
mrmazur1 <michaelrmaz@gmail.com>
2024-04-16 19:10:16 -0600

    fixed bug with managing events or out of swipes

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ def save_cookie(driver):
 
 
 if __name__ == "__main__":
-    #TODO make way to quit when constantly throwing exceptions
     count = int(input("how many profiles do you want to run: "))
     option = int(input("what do you want to do? 1 (save cookie), 2 (run normally)"))
     #count = 10
     exit_flag = False
-    bl = bumbleLoader(modelType='201', modelPath='models/dense_201_64_70_adam_.pth', arch='dense')
+    bl = bumbleLoader(modelType='201', modelPath='models/dense_201_64_130_adam_.pth', arch='dense')
     if option == 1:
         save_cookie(bl.driver)
     start = time.monotonic()
@@ -31,25 +30,19 @@ if __name__ == "__main__":
                 bl.load()
             bl.start(0, num_swipes=count)
         except Exception as e:
-            html = bl.driver.page_source
-            bl.driver.save_screenshot("web_page_screenshot.png")
-            val,nlikes, ndislikes = bl.tracker, bl.numLikes, bl.numDislikes
-            print(f"restarting at count of {val}")
-            bl.driver.get("https://www.google.com/")
-            bl.driver.get("https://bumble.com")
-            bl.load()
-            bl.driver.quit()
-            with open("web_page_source.html", "w", encoding="utf-8") as file:
-                file.write(html)
-                # print(e)
-                traceback.print_exc()
-            #TODO: fix crash after "caught up" screen
-            #the following code crashes due to looking for a non-existent object during bl.load()
             try:
-                bl = bumbleLoader(modelType='201', modelPath='models/dense_201_64_70_adam_.pth', arch='dense')
+                html = bl.driver.page_source
+                with open("web_page_source.html", "w", encoding="utf-8") as file:
+                    file.write(html)
+                    traceback.print_exc()
+                bl.driver.save_screenshot("web_page_screenshot.png")
+                val,nlikes, ndislikes = bl.tracker, bl.numLikes, bl.numDislikes
+                print(f"restarting at count of {val}")
+                bl.driver.get("https://www.google.com/")
+                bl.driver.get("https://bumble.com")
                 bl.load()
                 bl.start(val, numLikes=nlikes, numDislikes=ndislikes, num_swipes=count - val)
-            except TimeoutException as e:
+            except Exception as e:
                 print("most likely came to end of profiles in area")
                 break
     print(f"ending after completing {bl.tracker} profiles with {bl.numLikes} likes and {bl.numDislikes} dislikes")
